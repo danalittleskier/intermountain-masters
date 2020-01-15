ce37fe62f1f1e6b4a6259db98f36790a6bda3272
DanaLittleSkier <dana.alexandrescu@gmail.com>
2020-01-15 14:08:11 -0700

    Created a Parallax component

---
+++ src/components/Parallax/Parallax.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Parallax } from "react-materialize";
+
+function ParallaxComponent() {
+
+    return (
+        <div>
+        <Parallax
+            image={<img alt="" src="./images/StMoritz.jpg" />}
+            options={{
+                responsiveThreshold: 0
+            }}
+        />
+        <div className="section white">
+            <div className="row container">
+                <p className="grey-text text-darken-3 lighten-3">
+                    <img src="./images/intermountainmasters.png" alt="" />
+                </p>
+            </div>
+        </div>
+        <Parallax
+            image={<img alt="" src="./images/StMoritz.jpg" />}
+            options={{
+                responsiveThreshold: 0
+            }}
+        />
+    </div>
+    )
+    
+}
+
+export default ParallaxComponent;
