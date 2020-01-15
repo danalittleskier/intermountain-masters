14853e631a9d9292dca5e000ad3e0f1e6e267172
DanaLittleSkier <dana.alexandrescu@gmail.com>
2020-01-15 14:07:17 -0700

    Updated Carousel and added new images

--- src/components/Carousel/PicsCarousel.js
+++ src/components/Carousel/PicsCarousel.js
@@ -10,14 +10,14 @@ return (
     './images/powder-ski.jpg',
     './images/StMoritz.jpg',
     './images/tour.jpg',
-    'https://picsum.photos/200/300?image=3',
-    'https://picsum.photos/200/300?image=4'
+    './images/2019BIBS.jpg',
+    './images/GrandTarghee.jpg'
   ]}
   options={{
     dist: -100,
     duration: 200,
-    fullWidth: false,
-    indicators: false,
+    fullWidth: true,
+    indicators: true,
     noWrap: false,
     numVisible: 5,
     onCycleTo: null,
